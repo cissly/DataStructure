0b838e8217b06c15e8c2330dfda722db9253941a
cissly <tkdwls237@naver.com>
2023-03-29 21:17:36 +0900

    주석

--- DS_03_20200812_이상진_P3.1/ds3.1/ds3.1/FileName.c
+++ DS_03_20200812_이상진_P3.1/ds3.1/ds3.1/FileName.c
@@ -6,6 +6,7 @@ typedef struct node
 	struct node* link;
 }Node;
 
+//하나의 노드를 만들고 그 노드의 주소를 리턴하는 함수
 Node* Create_Node(int newdata)
 {
 	Node* newnode;
@@ -15,6 +16,7 @@ Node* Create_Node(int newdata)
 	return newnode;
 }
 
+//노드의 주소를 받아서 동적할당을 풀어주는 함수
 void Destroy_Node(Node* node)
 {
 	if (node != NULL)
@@ -24,6 +26,8 @@ void Destroy_Node(Node* node)
 	node = NULL;
 }
 
+/*더블포인터인 헤드를 받아 헤드가 가리키는 노드가 없다면 헤드가 노드를 가리키게 하거나
+연결리스트의 맨마지막에 노드를 추가시키는 함수*/
 void Append_Node(Node** head, Node* newnode)
 {
 	if (*head == NULL)
@@ -41,7 +45,7 @@ void Append_Node(Node** head, Node* newnode)
 	}
 }
 
-
+//연결리스트 출력함수
 void Print_list(Node* head)
 {
 	Node* iter = head;
